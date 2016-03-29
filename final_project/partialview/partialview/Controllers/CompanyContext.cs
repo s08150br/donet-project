b93733109ddfe6b7a94c81e4e71f05e7bec8c23c
s08150br <s08150br@gmail.com>
2016-03-29 14:46:26 -0400

    1
    1

---
+++ final_project/partialview/partialview/Controllers/CompanyContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace partialview.Controllers
+{
+    public class CompanyContext : Controller
+    {
+        // GET: CompanyContext
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
