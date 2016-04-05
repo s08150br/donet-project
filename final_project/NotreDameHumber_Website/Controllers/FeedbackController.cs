ac90534b6660ded78f58a08886e730ff348c90a0
s08150br <s08150br@gmail.com>
2016-04-05 19:11:49 -0400

    add feedback
    add feedback

---
+++ final_project/NotreDameHumber_Website/Controllers/FeedbackController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NotreDameHumber_Website.Controllers
+{
+    public class FeedbackController : Controller
+    {
+        private HDHDBContext db = new HDHDBContext();
+
+        // GET: Feedback
+        public ActionResult Index()
+        {
+            return View(db.FeedBacks.ToList());
+        }
+
+        // GET: Feedback/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FeedBack feedBack = db.FeedBacks.Find(id);
+            if (feedBack == null)
+            {
+                return HttpNotFound();
+            }
+            return View(feedBack);
+        }
+
+        // GET: Feedback/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Feedback/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "FeedbackId,FirstName,LastName,Phone,Age,Feedback1")] FeedBack feedBack)
+        {
+            if (ModelState.IsValid)
+            {
+                db.FeedBacks.Add(feedBack);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(feedBack);
+        }
+
+        // GET: Feedback/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FeedBack feedBack = db.FeedBacks.Find(id);
+            if (feedBack == null)
+            {
+                return HttpNotFound();
+            }
+            return View(feedBack);
+        }
+
+        // POST: Feedback/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "FeedbackId,FirstName,LastName,Phone,Age,Feedback1")] FeedBack feedBack)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(feedBack).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(feedBack);
+        }
+
+        // GET: Feedback/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FeedBack feedBack = db.FeedBacks.Find(id);
+            if (feedBack == null)
+            {
+                return HttpNotFound();
+            }
+            return View(feedBack);
+        }
+
+        // POST: Feedback/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            FeedBack feedBack = db.FeedBacks.Find(id);
+            db.FeedBacks.Remove(feedBack);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
