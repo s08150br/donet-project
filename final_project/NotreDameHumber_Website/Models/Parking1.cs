a9051884cc5cc9438ee13e0ae4102163ceeee4ae
s08150br <s08150br@gmail.com>
2016-04-13 18:05:56 -0400

    fix it
    fix it

--- final_project/NotreDameHumber_Website/Models/Parking1.cs
+++ final_project/NotreDameHumber_Website/Models/Parking1.cs
@@ -5,27 +5,29 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace NotreDameHumber_Website.Models
 {
     public class Parking1
     {
-        [Required(ErrorMessage = "Không được bỏ trống")]
-        [RegularExpression("\\d+", ErrorMessage = "Phải là số")]
-        [Range(1000, 2000, ErrorMessage = "ID phải từ 1000-2000")]
+        [Required(ErrorMessage = "Do not be left empty")]
+        [RegularExpression("\\d+", ErrorMessage = "Please inter cars number")]
+        [Range(1,100000, ErrorMessage = "ID must be number")]
         public int item_number { get; set; }
 
-        [Required(ErrorMessage = "Không được bỏ trống")]
-        [StringLength(100, MinimumLength = 5, ErrorMessage = "Tên sản phẩm từ 5 đến 100 ký tụ")]
+        [Required(ErrorMessage = "Please inter your name")]
+        [StringLength(100, MinimumLength = 5, ErrorMessage = "Please inter your name")]
         public string item_name { get; set; }
 
-        [Required(ErrorMessage = "Không được bỏ trống")]
-        [Range(1.0, 100.0, ErrorMessage = "Giá từ 1-100 USD")]
+        //[Required(ErrorMessage = "Do not be left empty")]
+        //[Range(1.0, 100.99, ErrorMessage = " 1-100 USD")]
+        [HiddenInput(DisplayValue =false)]
         public double amount { get; set; }
 
-        [Required(ErrorMessage = "Không được bỏ trống")]
-        [RegularExpression("\\d+", ErrorMessage = "Phải là số")]
-        [Range(1, 100, ErrorMessage = "Số lượng phải từ 1-100")]
+        [Required(ErrorMessage = "How many hours you want to stay ?")]
+        [RegularExpression("\\d+", ErrorMessage = "ust be a number")]
+        [Range(1, 24, ErrorMessage = "The number must be between 1-24")]
         public int quantity { get; set; }
         public int hours { get; set; }
     }
