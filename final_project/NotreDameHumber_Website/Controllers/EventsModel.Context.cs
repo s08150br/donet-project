8ba00691637d6bc6dc2c73c4ac4f155004224052
Alexander <alex.volynchikov@mail.ru>
2016-04-17 20:48:39 -0400

    Revert "Added my tables and my controllers"
    This reverts commit 08b94fb969e8ec3958c42a84b9fe39cb4e7cc439.

--- final_project/NotreDameHumber_Website/Controllers/EventsModel.Context.cs
+++ final_project/NotreDameHumber_Website/Controllers/EventsModel.Context.cs
@@ -36,9 +36,5 @@ namespace NotreDameHumber_Website.Controllers
         public DbSet<NewsLetter> NewsLetters { get; set; }
         public DbSet<Job> Jobs { get; set; }
         public DbSet<Worker> Workers { get; set; }
-        public DbSet<tblDoctor> tblDoctors { get; set; }
-        public DbSet<tblDonation> tblDonations { get; set; }
-        public DbSet<tblFAQ> tblFAQs { get; set; }
-        public DbSet<Donationtbl> Donationtbls { get; set; }
     }
 }
