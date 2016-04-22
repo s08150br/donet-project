35dfc4ed0c3d59e79ee7aff08da244ad03ffe3d3
n01122242@humbermail.ca <susan.saler@hotmail.com>
2016-04-22 16:25:42 -0500

    submit
    submit

--- final_project/NotreDameHumber_Website/Models/Parking1.cs
+++ final_project/NotreDameHumber_Website/Models/Parking1.cs
@@ -12,12 +12,12 @@ namespace NotreDameHumber_Website.Models
     public class Parking1
     {
         [Required(ErrorMessage = "Do not be left empty")]
-        [RegularExpression("\\d+", ErrorMessage = "Please inter cars number")]
+        [RegularExpression("\\d+", ErrorMessage = "Please enter number")]
         [Range(1,100000, ErrorMessage = "ID must be number")]
         public int item_number { get; set; }
 
-        [Required(ErrorMessage = "Please inter your name")]
-        [StringLength(100, MinimumLength = 1, ErrorMessage = "Please inter your name")]
+        [Required(ErrorMessage = "Please enter your name")]
+        [StringLength(100, MinimumLength = 1, ErrorMessage = "Please enter your name")]
         public string item_name { get; set; }
 
         //[Required(ErrorMessage = "Do not be left empty")]
@@ -25,8 +25,8 @@ namespace NotreDameHumber_Website.Models
         [HiddenInput(DisplayValue =false)]
         public double amount { get; set; }
 
-        [Required(ErrorMessage = "How many hours you want to stay ?")]
-        [RegularExpression("\\d+", ErrorMessage = "ust be a number")]
+        [Required(ErrorMessage = "How many hours do you want to stay ?")]
+        [RegularExpression("\\d+", ErrorMessage = "must be a number")]
         [Range(1, 24, ErrorMessage = "The number must be between 1-24")]
         public int quantity { get; set; }
         public int hours { get; set; }
