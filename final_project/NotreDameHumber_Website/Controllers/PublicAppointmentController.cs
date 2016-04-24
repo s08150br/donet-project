9976969802277b3b24706e1387d21f718751a90d
fatemehabdizadeh <n01137646@humbermail.ca>
2016-04-24 13:02:00 -0400

    Public side of Online Appointment
    Done

---
+++ final_project/NotreDameHumber_Website/Controllers/PublicAppointmentController.cs
@@ -0,0 +1,32 @@
+﻿using NotreDameHumber_Website.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NotreDameHumber_Website.Controllers
+{
+    public class PublicAppointmentController : Controller
+    {
+        NotreDameContext ndContext = new NotreDameContext();
+        // GET: PublicAppointment
+        public ActionResult Appointment()
+        {
+            ViewBag.Services = ndContext.Services;
+            ViewBag.Therapists = ndContext.Therapists;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Appointment(Appointment appointment)
+        {
+            appointment.Set = "No";
+            ndContext.Appointments.Add(appointment);
+            ndContext.SaveChanges();
+            ViewBag.Message = "Dear " + appointment.Firstname + " " + appointment.Lastname + " you have Successfully made an appointment with us.We will call you within 24 hours to set the appointment with you.";
+            ViewBag.Services = ndContext.Services;
+            ViewBag.Therapists = ndContext.Therapists;
+            return View();
+        }
+    }
+}
