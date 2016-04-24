404a0bd14e16379011244f9248295393f7f7c683
fatemehabdizadeh <n01137646@humbermail.ca>
2016-04-24 18:19:56 -0400

    Dynamic Menu
    In progress

---
+++ final_project/NotreDameHumber_Website/Controllers/MenusController.cs
@@ -0,0 +1,135 @@
+﻿using NotreDameHumber_Website.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NotreDameHumber_Website.Controllers
+{
+    public class MenusController : Controller
+    {
+        MenusContext db = new MenusContext();
+        // GET: Menus
+        public ActionResult Index()
+        {
+            return View(db.Menus.ToList());
+        }
+        public void FindChildMenu(int id)
+        {
+            TempData["Count"] = db.Menus.Where(m => m.ParentId == id).Count();
+        }
+
+        [ChildActionOnly]
+        public ActionResult TopMenu()
+        {
+            return PartialView("_HeaderMenu", db.Menus);
+        }
+
+        // GET: Menus/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Menu menu = db.Menus.Find(id);
+            if (menu == null)
+            {
+                return HttpNotFound();
+            }
+            return View(menu);
+        }
+
+        // GET: Menus/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Menus/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,MenuName,ParentId,Action,Controller")] Menu menu)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Menus.Add(menu);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(menu);
+        }
+
+        // GET: Menus/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Menu menu = db.Menus.Find(id);
+            if (menu == null)
+            {
+                return HttpNotFound();
+            }
+            return View(menu);
+        }
+
+        // POST: Menus/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,MenuName,ParentId,Action,Controller")] Menu menu)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(menu).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(menu);
+        }
+
+        // GET: Menus/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Menu menu = db.Menus.Find(id);
+            if (menu == null)
+            {
+                return HttpNotFound();
+            }
+            return View(menu);
+        }
+
+        // POST: Menus/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Menu menu = db.Menus.Find(id);
+            db.Menus.Remove(menu);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
